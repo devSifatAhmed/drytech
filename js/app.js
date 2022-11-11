19166b0c506ba6ea2605a05f263991082a481b48
programmer-sifat-ahmed <programmer.sifatahmed@gmail.com>
2022-11-11 04:26:29 -0500

    slider section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
-    $(".about-wrapper > .col > .video > .thumb").click(function(){
+$(document).ready(function () {
+    $(".about-wrapper > .col > .video > .thumb").click(function () {
         $(this).addClass("active");
         $(this).next().attr("controls", "");
+    });
+
+
+
+
+
+    // slider
+    $('.slides').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
     });
 });
